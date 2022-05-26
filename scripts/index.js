4ad0e0e4b43b7472313451f92fee04615761c759
NatalieShamir <nataliashamir85@gmail.com>
2022-05-26 14:31:58 +0300

    feat: code gallery section

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,26 @@
-//Declarations
-const initialPhotos = [
-  {
-    name: "",
-    link: "",
-  },
-  {
-    name: "",
-    link: "",
-  },
-  {
-    name: "",
-    link: "",
-  },
-  {
-    name: "",
-    link: "",
-  },
-  {
-    name: "",
-    link: "",
-  },
-];
+const gallery = document.getElementById("gallery-items");
+const popup = document.getElementById("popup");
+const popupImage = document.getElementById("popup-image");
+const ImageIndexes = [1, 2, 3, 4, 5];
+const popupImageIndex = null;
 
-//Modals
+ImageIndexes.forEach((i) => {
+  const image = document.createElement("img");
+  image.src = `./images/gallery/Terry__gallery-${i}.png`;
+  image.alt = `Illusration ${i} of Terrence Shlomo Tegegne as presented in the interactive gallery on his landing page`;
+  image.classList.add("gallery__image");
+
+  image.addEventListener("click", () => {
+    popup.style.transform = `translateY(0)`;
+    popupImage.src = `./images/gallery/Terry__gallery-${i}.png`;
+    popupImage.alt = `Illusration ${i} of Terrence Shlomo Tegegne as presented in the interactive gallery on his landing page`;
+  });
+
+  gallery.appendChild(image);
+});
+
+popup.addEventListener("click", () => {
+  popup.style.transform = `translateY(-100%)`;
+  popup.src = "";
+  popup.alt = "";
+});
